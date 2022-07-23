1db40ff52a4f05262670083a59c407deb2cf12ef
Екатерина Рыпта <ekaterinarypta@MacBook-Air-Ekaterina.local>
2022-07-23 16:08:32 +0300

    added basic structure for routings

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { Route } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import Header from "./components/Header";
+import Home from "./Pages/Home/Home";
+import Quiz from "./Pages/Quiz/Quiz";
+import Settings from "./Pages/Settings/Settings";
+
 const App = () => {
-	return <div>asdasbdhgasghjdas</div>;
+  return (
+    <div className="wrapper">
+      <Header />
+      <Navigation />
+      <Route path="/Home" element={<Home />} />
+      <Route path="/Quiz" element={<Quiz />} />
+      <Route path="/Settings" element={<Settings />} />
+    </div>
+  );
 };
 
 export default App;
